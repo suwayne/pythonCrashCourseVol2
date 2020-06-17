070805bb70d3032d2adc90bdc71ba4171f16c0cb
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-06-17 02:40:42 +0100

    completed ch8-ex8

--- chapter8/8-8.py
+++ chapter8/8-8.py
@@ -1,4 +1,4 @@
-def make_album(artist, title,):
+def make_album(artist, title, tracks=0):
     """build a dictionary containing info about an album"""
     album_dict = {
         'artist': artist.title(),
@@ -8,4 +8,24 @@ def make_album(artist, title,):
         album_dict['tracks'] = tracks
     return album_dict
 
-# prepare the prompts as expected.
+
+# prepare the prompts.
+title_prompt = "\nwhat album are you thinking of?"
+artist_prompt = "who is the artist?"
+
+# let the user know when it's time to quit.
+print("Enter 'quit' at any time to stop.")
+
+while True:
+    title = input(title_prompt)
+    if title == 'quit':
+        break
+
+    artist = input(artist_prompt)
+    if artist == 'quit':
+        break
+
+    album = make_album(artist, title)
+    print(album)
+
+print("\nThanks for responding.")
