0c99969bb224ab311f19ea41ce2c37d2bc488ce4
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-06-19 00:19:46 +0100

    new commit

---
+++ chapter8/8-11.py
@@ -0,0 +1,31 @@
+# lesson 8-11: archived messages
+messages = [
+    "i'm on pcc chapter8",
+    "coding is addictive",
+    "lets crush what's left",
+]
+sent_messages = []
+
+
+def send_messages(messages, sent_messages):
+    """
+    simulate printing each message, until none are left.
+    move each message to sent_messages.
+    """
+    print("\nThe following messages have been printed:")
+    while messages:
+        moved_messages = messages.pop()
+        print(f"Sent messages: {moved_messages}")
+        sent_messages.append(moved_messages)
+
+
+send_messages(messages, sent_messages)
+
+print("\nFinal lists:")
+print(messages)
+print(sent_messages)
+
+send_messages(messages[:], sent_messages[:])
+print("\nbacked up list")
+print(messages)
+print(sent_messages)
