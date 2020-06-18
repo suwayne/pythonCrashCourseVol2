ad501b35bac94b141b81e88d392ed367a7709485
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-06-18 17:02:44 +0100

    solution to 8-9

---
+++ chapter8/8-9.py
@@ -0,0 +1,14 @@
+messages = [
+    "i'm on pcc chapter8",
+    "coding is addictive",
+    "lets crush what's left",
+]
+
+
+def show_messages(messages):
+    "print all the messages in a list."
+    for message in messages:
+        print(message)
+
+
+show_messages(messages)
