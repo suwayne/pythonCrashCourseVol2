8cae8ba05f6d2f1138eeb72f75c68e099f7919e9
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-06-18 10:34:02 +0100

    new commit

---
+++ chapter8/printing_models2.py
@@ -0,0 +1,5 @@
+def print_models(unprinted_designs, completed_models):
+    """
+    simulate printing each design, until none are left. 
+    move each design to completed_models after printing.
+    """
