3d35469066314c06e4cf72c5a0e3207446668faf
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-06-26 23:57:57 +0100

    new commit

---
+++ chapter8/8-14.py
@@ -0,0 +1,8 @@
+def car():
+
+
+def make_car(manufacturer, model, **options):
+    """make a dictionary representing a car."""
+    car_dict = {
+        'manufacturer': manufacturer.title(),
+    }
