b944eeb0d649304d449e0f9b7e9ead002e35416f
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-06-29 15:13:01 +0100

    importing entire modules

--- chapter8/pizza.py
+++ chapter8/pizza.py
@@ -3,7 +3,3 @@ def make_pizza(*toppings):
     print("\nMaking a pizza with the following toppings:")
     for topping in toppings:
         print(f" -{topping}")
-
-
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
