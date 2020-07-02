495260bd9a5b1e60dcfe40f03af53d5195150d13
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-02 23:56:45 +0100

    understanding how classes work

--- chapter9/dog.py
+++ chapter9/dog.py
@@ -13,3 +13,9 @@ class Dog:
     def roll_over(self):
         """simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
+
+
+my_dog = Dog('Willie', 6)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog's age is {my_dog.age} years old.")
