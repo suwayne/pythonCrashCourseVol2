901492c6e94a76ab7970c6a1405a31b31a9ba9af
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-02 18:22:22 +0100

    code recovered

---
+++ challenges/playingcards3-1.py
@@ -0,0 +1,8 @@
+hearts = ['A of Hearts', '2 of Heards', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of hearts',
+          '7 of Hearts', '8 of Hearts', '9 of Hearts', '10 of Hearts', 'J of Hearts', 'Q of Hearts', 'K of Hearts']
+
+play_hearts = []
+
+play_hearts.append(hearts[0:3])
+
+print(play_hearts)
