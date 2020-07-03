349a232e1a2d8566381927e529bd456253388c91
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-03 02:25:09 +0100

    ch4.heart.loops.challenge.completed

---
+++ challenges/playingcards4-1.py
@@ -0,0 +1,14 @@
+hearts = ['A of Hearts', '2 of Heards', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of hearts',
+          '7 of Hearts', '8 of Hearts', '9 of Hearts', '10 of Hearts', 'J of Hearts', 'Q of Hearts', 'K of Hearts']
+
+# """loop through list, and print out all cards in order"""
+# for heart in hearts:
+#     print(heart)
+
+# """loop through list, and print out just cards with numbers on them"""
+# for heart in hearts[1:10]:
+#     print(heart)
+
+"""loop throught list and print out just face cards"""
+for heart in hearts[10:13]:
+    print(heart)
