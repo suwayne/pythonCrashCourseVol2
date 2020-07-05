d90b61d8f1b85ae9866128c438ea2ad34486b7a2
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-05 17:27:50 +0100

    update

---
+++ challenges/playingcards4-3.py
@@ -0,0 +1,6 @@
+hearts = ['A of hearts', '2 of hearts', '3 of hearts', '4 of hearts', '5 of hearts', '6 of hearts',
+          '7 of hearts', '8 of hearts', '9 of hearts', '10 of hearts', 'J of hearts', 'Q of hearts', 'K of hearts']
+
+diamonds = []
+clubs = []
+spades = []
