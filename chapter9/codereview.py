ebccf57fe34b6929023a7ac03a42ab1bb9aaa0e2
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-09 04:42:19 +0100

    new commit

---
+++ chapter9/codereview.py
@@ -0,0 +1,9 @@
+def __init__(self, name, age):
+    """initialize name and age attributes"""
+    self.name = name
+    self.age = age
+
+
+def sit(self):
+    """simulate a dog sitting in response to a command"""
+    print(f"{self.name} iis not sitting.")
