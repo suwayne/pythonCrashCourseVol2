07422095e5d4f7e34ecb31cfb2b4988249d505a4
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-10 11:05:17 +0100

    friday code review

--- chapter9/codereview.py
+++ chapter9/codereview.py
@@ -1,9 +1,20 @@
-def __init__(self, name, age):
-    """initialize name and age attributes"""
-    self.name = name
-    self.age = age
+class Dog:
+
+    def __init__(self, name, age):
+        """initialize name and age attributes"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """simulate rolling over in response to a command"""
+        print(f"{self.name} rolled over!")
 
 
-def sit(self):
-    """simulate a dog sitting in response to a command"""
-    print(f"{self.name} iis not sitting.")
+the_dog = Dog('Willie', 6)
+
+print(f"My dog's name is {the_dog.name}.")
+print(f"My dog is {the_dog.age} years old.")
