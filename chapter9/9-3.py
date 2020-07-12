7154b494826dc8fea47abc47bb52adf4ac2afa86
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-12 17:08:30 +0100

    new commit

--- chapter9/9-3.py
+++ chapter9/9-3.py
@@ -1,2 +1,10 @@
 class User():
     def __init__(self, first_name, last_name, age, height):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.height = height
+
+    def describe_user(self):
+        msg = f"{self.first_name} is from the {self.last_name} clan, {self.age} years old and {self.height} tall"
+        print(f"\n{msg}")
