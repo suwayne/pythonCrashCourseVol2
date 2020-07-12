5dd713d29fddcea4cd363d0f939d08b10e204829
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-12 13:20:58 +0100

    ch9.1

---
+++ chapter9/9-1.py
@@ -0,0 +1,9 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name}")
+        print(f"{self.cuisine_type}")
