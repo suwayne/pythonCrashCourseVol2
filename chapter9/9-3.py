021559bac61624ca001ebed14151df46951895b6
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-14 02:36:14 +0100

    exercise 9-3 completed

--- chapter9/9-3.py
+++ chapter9/9-3.py
@@ -1,4 +1,5 @@
 class User():
+
     def __init__(self, first_name, last_name, age, height):
         self.first_name = first_name.title()
         self.last_name = last_name.title()
@@ -6,7 +7,7 @@ class User():
         self.height = height
 
     def describe_user(self):
-        msg = f"{self.first_name} is from the {self.last_name} clan, {self.age} years old and {self.height} tall"
+        msg = f"{self.first_name} is from the {self.last_name} clan, {self.age} years old and {self.height} meters tall"
         print(f"\n{msg}")
 
     def greet_user(self):
@@ -14,5 +15,6 @@ class User():
         print(f"\n{msg}")
 
 
-iyayi = User('iyayi', 'ogbebor')
+iyayi = User('iyayi', 'ogbebor', 27, 180)
+iyayi.describe_user()
 iyayi.greet_user()
