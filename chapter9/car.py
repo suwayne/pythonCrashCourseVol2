bb5f3e500b6b04ac472599b03aeba184ce20b95e
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-15 00:15:07 +0100

    code update

--- chapter9/car.py
+++ chapter9/car.py
@@ -18,9 +18,16 @@ class Car():
         """print a statement showing the car's mileage"""
         print(f"This car has {self.odometer_reading} miles on it.")
 
+    def update_odometer(self, mileage):
+        """set the odometer reading to the given value"""
+        self.odometer_reading = mileage
+
 
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
 
-my_new_car.odometer_reading = 23
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
+
+my_new_car.update_odometer(23)
 my_new_car.read_odometer()
