1398f56381e1bf2b4d4dfd08097bf1724575f598
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-14 16:16:59 +0100

    code update

--- challenges/codingForSocialJustice/csj3.2.py
+++ challenges/codingForSocialJustice/csj3.2.py
@@ -1,5 +1,5 @@
 # Helping Organizations
-help_organizations = ['khan foundation', 'smile care', ]
+help_organizations = ['khan foundation', 'smile care', 'sega link']
 
 for help_org in help_organizations:
     print(help_org)
