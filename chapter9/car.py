b7ae5824dfcedbf6a7890a25ccd851cbd04f35ac
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-14 02:49:00 +0100

    car.py

---
+++ chapter9/car.py
@@ -0,0 +1,18 @@
+class Car():
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """initialize attributes to describe a car"""
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name"""
+        long_name = f"{self.make} {self.model} {self.year}"
+        return long_name.title()
+        # print(f"\n{long_name.title()}")
+
+
+my_new_car = Car('audi', 'a4', 2019)
+print(my_new_car.get_descriptive_name())
