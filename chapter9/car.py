74b8a520846349c6f4720e671b24dbb5dc45796e
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-16 11:02:28 +0100

    code update

--- chapter9/car.py
+++ chapter9/car.py
@@ -18,16 +18,30 @@ class Car():
         """print a statement showing the car's mileage"""
         print(f"This car has {self.odometer_reading} miles on it.")
 
+    # def update_odometer(self, mileage):
+    #     """set the odometer reading to the given value"""
+    #     self.odometer_reading = mileage
+
     def update_odometer(self, mileage):
-        """set the odometer reading to the given value"""
-        self.odometer_reading = mileage
+        """set the odometer reading to the given value
+           Reject the change if it attempts to roll the odometer
+           back        
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("you cant roll back an odometer!")
 
 
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
 
+
 # my_new_car.odometer_reading = 23
 # my_new_car.read_odometer()
 
 my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(25)
 my_new_car.read_odometer()
