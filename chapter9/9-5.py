9342bc6c5e2a2d5188da09f01f1b2b3b1a3c7a53
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-23 18:17:22 +0100

    code udpate

---
+++ chapter9/9-5.py
@@ -0,0 +1,29 @@
+class User():
+
+    def __init__(self, first_name, last_name, age, height, login_attempts):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.age = age
+        self.height = height
+        self.login_attempts = 0
+
+    def describe_user(self):
+        msg = f"{self.first_name} is from the {self.last_name} clan, {self.age} years old and {self.height} meters tall"
+        print(f"\n{msg}")
+
+    def greet_user(self):
+        msg = f"How are you doing today {self.first_name} {self.last_name}."
+        print(f"\n{msg}")
+
+    def increment_login_attempts(self):
+        """Increment the value of login attempts by 1"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """reset the value of login attempts to 0"""
+        self.login_attempts = 0
+
+
+iyayi = User('iyayi', 'ogbebor', 27, 180)
+iyayi.describe_user()
+iyayi.greet_user()
