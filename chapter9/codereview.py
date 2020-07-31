5b236a97fa3f8b3752ed5f7950a1a9b3e9067cea
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-07-31 12:09:49 +0100

    code update

--- chapter9/codereview.py
+++ chapter9/codereview.py
@@ -1,15 +1,32 @@
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
 
-    def sit(self):
-        print(f"{self.name} is now sitting.")
+    def get_descriptive_name(self):
+        """return neatly formatted descriptive name"""
+        long_name = f"{self.make} {self.model} {self.year}"
+        return long_name.title()
 
-    def roll_over(self):
-        print(f"{self.name} rolled over!")
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it")
+
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't rollback an odometer.")
 
 
-my_dog = Dog('Willie', 6)
+osasu = Car('toyota', 'avalon', 2019)
+print(osasu.get_descriptive_name())
 
-print(f"My dog's name is {my_dog.name")
+# set odometer from the method, and print a statement showing the cars mileage
+osasu.odometer_reading = 59
+osasu.read_odometer()
+
+osasu.update_odometer(3)
+osasu.read_odometer()
