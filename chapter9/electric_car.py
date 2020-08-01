3dd97f1682f2bcf6a3bcffeba994c24a69bbb674
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-08-01 23:20:00 +0100

    parent_and_child_class

--- chapter9/electric_car.py
+++ chapter9/electric_car.py
@@ -32,7 +32,13 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of a parent class."""
         super().__init__(make, model, year)
+        self.battery_size = 75
+
+    def describe_battery(self):
+        """print a statement describing battery size."""
+        print(f"This car has a {self.battery_size}-Kwhs battery")
 
 
 my_tesla = ElectricCar('tesla', 'model s', '2019')
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
