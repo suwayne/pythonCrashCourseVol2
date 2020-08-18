4c63833c37055bd0ce499c042ce96a70212166e8
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-08-19 00:33:20 +0100

    code update

--- chapter9/dog.py
+++ chapter9/dog.py
@@ -1,20 +1,20 @@
-class Dog:
+class Dog:  # class
     """A simple attempt to model a dog."""
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-    def sit(self):
+    def sit(self):  # method definition
         """Simulate a dog sitting in response to a command."""
         print(f"{self.name} is now sitting.")
 
-    def roll_over(self):
+    def roll_over(self):  # method definition
         """Simulate a roll over response to a command."""
         print(f"{self.name} rolled over!")
 
 
-my_dog = Dog('Rocky', 6)
+my_dog = Dog('Rocky', 6)  # instance of a class
 
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
