1b6b4f32f4fcda8f90178bc6e0860f71152fdd22
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-08-20 02:36:46 +0100

    ex9-1

---
+++ chapter9/9-1.py
@@ -0,0 +1,17 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"The restaurant is called {self.restaurant_name}.")
+        print(f"They serve {self.cuisine_type}.")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open for business.")
+
+
+restaurant = Restaurant('kfc', 'fast food')
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
