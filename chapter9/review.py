dc4df29f150d76b8d15d783a04b2c17d76894e4f
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-08-20 22:33:43 +0100

    code_review

--- chapter9/review.py
+++ chapter9/review.py
@@ -5,12 +5,22 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name."""
         long_name = f"{self.make} {self.model} {self.year}."
         return long_name.title()
 
+    def read_odometer(self):
+        """Print statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
 
 my_new_car = Car('BMW', '3i', '2018')
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+# modifying an attributes value
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
