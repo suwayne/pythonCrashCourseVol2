53296592b1c570b1be3dd1fa8c1df7c398111f61
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-08-24 00:18:46 +0100

    revision

---
+++ chapter9/revision.py
@@ -0,0 +1,18 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(
+            f"Welcome to {self.restaurant_name} and we serve freshly made {self.cuisine_type}.")
+
+    def open_restaurant(self):
+        print(f"The {self.restaurant_name} is open for orders.")
+
+
+restaurant = Restaurant('Sweet Sensation', 'urban')
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+# restaurant.describe_restaurant()
