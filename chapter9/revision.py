86e746b454c7b28542c54421f116e8d43e0c40d3
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-08-27 23:24:28 +0100

    code update

--- chapter9/revision.py
+++ chapter9/revision.py
@@ -8,7 +8,12 @@ class Restaurant:
             f"Welcome to {self.restaurant_name} and we serve freshly made {self.cuisine_type}.")
 
     def open_restaurant(self):
-        print(f"The {self.restaurant_name} is open for orders.")
+        print(f"The {self.restaurant_name} restaurant is open for orders.")
 
 
 restaurant = Restaurant('Sweet Sensation', 'urban')
+restaurant.describe_restaurant()
+
+mainland = Restaurant('Mr Biggs', 'Fast food')
+mainland.describe_restaurant()
+mainland.open_restaurant()
