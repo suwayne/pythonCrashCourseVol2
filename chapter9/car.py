c90912214307af77b1660631015c78736bc642a3
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-08-28 01:47:34 +0100

    code update

--- chapter9/car.py
+++ chapter9/car.py
@@ -24,20 +24,30 @@ class Car:
         else:
             print("You can't roll back an odometer!")
 
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
 
 my_new_car = Car('BMW', '3i', '2018')
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
 
 # modifying an attributes value directly
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
 
-my_new_car.update_odometer(40)
-my_new_car.read_odometer()
+# my_new_car.update_odometer(40)
+# my_new_car.read_odometer()
 
-my_new_car.update_odometer(300)
-my_new_car.read_odometer()
+# my_new_car.update_odometer(300)
+# my_new_car.read_odometer()
 
-my_new_car.update_odometer(3)
-my_new_car.read_odometer()
+# my_new_car.update_odometer(3)
+# my_new_car.read_odometer()
+
+
+my_used_car = Car('Honda', 'Accord', '2008')
+my_used_car.get_descriptive_name()
+
+my_used_car.update_odometer(670)
+my_used_car.read_odometer()
