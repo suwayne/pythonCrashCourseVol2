8719b3de1f17186b05d2216d04fcc3900c94cc9a
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-09-03 23:27:27 +0100

    code update

--- chapter9/9-5.py
+++ chapter9/9-5.py
@@ -4,13 +4,24 @@ class User:
         self.last_name = last_name
         self.age = age
         self.signup_date = signup_date
-        self.login_attempts = login_attempts
+        self.login_attempts = 0
 
     def describe_user(self):
         """summary of the user's information"""
         print(
             f"The user {self.first_name} {self.last_name} is {self.age} years old and signed up on {self.signup_date}")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
 
 iyayi = User('Iyayi', 'Ogbebor', 32, 'Feb 12')
 iyayi.describe_user()
+
+print(f"Number of login attempts:")
+iyayi.increment_login_attempts()
+iyayi.increment_login_attempts()
+iyayi.increment_login_attempts()
