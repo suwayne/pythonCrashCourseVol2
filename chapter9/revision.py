d100b5e0a93622c5616f6ca83f7bab2e14406b81
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-09-04 23:38:00 +0100

    code update

--- chapter9/revision.py
+++ chapter9/revision.py
@@ -1,20 +1,20 @@
 class User:
     def __init__(self, user_name, profession, email_address):
-        self.user_name = user_name.title()
-        self.profession = profession.title()
+        self.user_name = user_name
+        self.profession = profession
         self.email_address = email_address
 
 
-def describe_user(self):
-    print(f"User details:")
-    print(f"\t{self.user_name}")
-    print(f"\t{self.profession}")
-    print(f"\t{self.email_address}")
+# def describe_user(self):
+#     print(f"User details:")
+#     print(f"\t{self.user_name}")
+#     print(f"\t{self.profession}")
+#     print(f"\t{self.email_address}")
 
 
 def greet_user(self):
     print(f"Hello! {self.user_name}, we are happy to have you.")
 
 
-ehis = User('ehisumwen ogbebor',
-            'maintenance programmer', 'ehis_jr@yahoo.com')
+sasu = User('ehisumwen ogbebor', 'maintenance programmer', 'ehis_jr@yahoo.com')
+sasu.greet_user()
