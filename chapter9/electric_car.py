9bea3b944254b29fc5aba46dd34d38fccaa8afa7
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-09-13 17:12:32 +0100

    inheritance

--- chapter9/electric_car.py
+++ chapter9/electric_car.py
@@ -28,18 +28,25 @@ class Car:
         self.odometer_reading += miles
 
 
-class ElectricCar(Car):
-    def __init__(self, make, model, year):
-        """initialize attriutes of the parent class."""
-        super().__init__(make, model, year)
-        """initialize attributes specific to an electric car."""
-        self.battery_size = 75
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=75):
+        """Initialize battery's attributes."""
+        self.battery_size = battery_size
 
     def describe_battery(self):
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-KWh battery.")
 
 
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        """initialize attriutes of the parent class."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
