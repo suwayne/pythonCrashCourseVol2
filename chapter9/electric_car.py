5f87f01761b80eac14655d44f53a3a8aacdd554b
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-09-17 23:52:44 +0100

    code update

--- chapter9/electric_car.py
+++ chapter9/electric_car.py
@@ -46,6 +46,10 @@ class ElectricCar(Car):
         super().__init__(make, model, year)
         self.battery = Battery()
 
+    def fill_gas_tank(self):
+        "electric car's dont have gas tanks"
+        print("This car doesn't need a gas tank!")
+
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
