79fb251b31dad08ca083d206dcc434dbd710c6f4
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-09-20 03:25:43 +0100

    ex 9-8 in progress

---
+++ chapter9/9-8.py
@@ -0,0 +1,38 @@
+class User:
+    def __init__(self, first_name, last_name, age, signup_date):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.signup_date = signup_date
+
+    def describe_user(self):
+        """summary of the user's information"""
+        print(
+            f"The user {self.first_name} {self.last_name} is {self.age} years old and signed up on {self.signup_date}")
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age, signup_date):
+        super().__init__(first_name, last_name, age, signup_date)
+        self.privileges = [
+            'can add post',
+            'can delete post',
+            'can ban user'
+        ]
+
+"""continue from here
+class Privileges(Admin):
+    def __init__(self, privileges):
+        self.privileges = [
+            'can add post',
+            'can delete post',
+            'can ban user'
+        ]
+
+    def show_privileges(self):
+        print(f"\nHere is a list of the Administrators privileges:")
+        for privilege in self.privileges:
+            print(f"-{privilege.title()}")
+
+
+osasu = Privileges
