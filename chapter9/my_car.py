8fd912e3f9a6dd5dd68e380fc1b40e520cee2fdc
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-09-25 22:46:57 +0100

    code update

--- chapter9/my_car.py
+++ chapter9/my_car.py
@@ -1,3 +1,5 @@
+# if you are going to import and use your class as an alias
+from electric_car import Car as CA
 from car import Car
 
 my_new_car = Car('audi', 'a4', 2019)
@@ -5,3 +7,7 @@ print(my_new_car.get_descriptive_name())
 
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+
+osasu_car = CA('audi', 'a7', 2020)
+print(osasu_car.get_descriptive_name())
