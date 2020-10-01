733fb039f1ffc9f7e97d470defa27cfe01abc898
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-01 20:06:55 +0100

    new commit

--- chapter9/revision5.py
+++ chapter9/revision5.py
@@ -37,7 +37,24 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """initialize attributes of the parent class"""
         super().__init__(make, model, year)
+        self.battery_size = 75
+
+    def describe_battery(self):
+        """print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-KWh battery.")
+
+    """report the range of the car based on the battery size."""
+
+    def get_range(self):
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")
 
 
 osasu_tesla = ElectricCar('tesla', 'model s', 2019)
 print(osasu_tesla.get_descriptive_name())
+
+osasu_tesla.describe_battery()
+osasu_tesla.get_range()
