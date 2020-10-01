693edc7378234cbf832b9df2f06973a72abddd1c
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-01 03:51:28 +0100

    code udpate

--- chapter9/revision5.py
+++ chapter9/revision5.py
@@ -37,3 +37,7 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """initialize attributes of the parent class"""
         super().__init__(make, model, year)
+
+
+osasu_tesla = ElectricCar('tesla', 'model s', 2019)
+print(osasu_tesla.get_descriptive_name())
