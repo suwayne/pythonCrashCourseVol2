b5747392d532a51470306cb1bc443f0894d8ebe1
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-04 18:24:44 +0100

    code update

---
+++ chapter10/birthday_pi.py
@@ -0,0 +1,14 @@
+filename = '/Users/osasumwenogbebor/Documents/dev/pythonCrashCourseVol2/chapter10/longer_pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first thousand digits of pi.")
+else:
+    print("Your birthday does not appear in the first thousand digits of pi.")
