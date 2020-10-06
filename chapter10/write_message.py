16a0cc5def6595f82459075dde5b4926e8388f53
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-06 22:27:42 +0100

    new commit

---
+++ chapter10/write_message.py
@@ -0,0 +1,7 @@
+filename = 'programming.txt'
+
+# with open(filename, 'w') as file_object:
+with open(filename, 'w') as file_object:
+
+    file_object.write("I love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
