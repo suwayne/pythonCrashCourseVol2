2f70032afcd9478b66bb10e1cfa5a451f9dbedcf
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-10 22:41:27 +0100

    code revision

--- chapter10/debug.py
+++ chapter10/debug.py
@@ -1,4 +1,7 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("you cannot divide by zero")
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("you cannot divide by zero")
+
+print("Give me two numbers, and i'll divide them")
+print("push 'q' to quit")
