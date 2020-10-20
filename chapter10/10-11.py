e3d7b7a48660784d0095a3e73d6aea66e72ef31f
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-20 03:13:38 +0100

    code update2

--- chapter10/10-11.py
+++ chapter10/10-11.py
@@ -2,6 +2,6 @@ import json
 
 question = input("what is your favourite number?: ")
 
-with open(answer, 'w') as f:
+with open('favorite_number.jsons', 'w') as f:
     json.dump(question, f)
     print(f"I know your favorite number is {question}.")
