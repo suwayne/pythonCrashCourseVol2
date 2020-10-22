b2361b2dd4c5948c5fd8af90d0398a03f05828e8
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-22 21:23:31 +0100

    code update

---
+++ chapter10/10-12.py
@@ -0,0 +1,16 @@
+import json
+
+try:
+    with open('favorite_number.json') as f:
+        number = json.load(f)
+
+except FileNotFoundError:
+
+    number = input("What is your favorite number? ")
+    with open('favorite_number.json', 'w') as f:
+        json.dump(number, f)
+
+    print(f"Thanks i'll remember that.")
+
+else:
+    print(f"I know your favorite number! It's {number}.")
