7b23fa7ed75fac6ff722d861577c57e6240acce2
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-24 01:40:22 +0100

    ch11

---
+++ chapter11/name_function.py
@@ -0,0 +1,7 @@
+def get_formatted_name(first, last):
+    # geerate a neatly formatted full name.
+    full_name = f"{first} {last}"
+    return full_name.title()
+
+
+print(get_formatted_name('osasu', 'ogbebor'))
