12523cfaaa603e66aa4b8950bc70a38c26b13daf
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-26 06:06:21 +0100

    test passed

--- chapter11/test_cities.py
+++ chapter11/test_cities.py
@@ -6,4 +6,8 @@ class CityCountryTest(unittest.TestCase):
 
     def test_city_country(self):
         formatted_output = get_city_country('santiago', 'chile')
-        self.assertEquals(formatted_output, 'Santiago Chile')
+        self.assertEquals(formatted_output, 'Santiago, Chile')
+
+
+if __name__ == '__main__':
+    unittest.main()
