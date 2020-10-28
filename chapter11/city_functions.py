8df69d8fc213b8a499346a17503cf17ad6fc6c8c
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-10-28 04:46:30 +0100

    new commit

--- chapter11/city_functions.py
+++ chapter11/city_functions.py
@@ -1,6 +1,6 @@
-def get_city_country(city, country):
-    full_country_description = f"{city}, {country}"
+def get_city_country(city, country, population):
+    full_country_description = f"{city}, {country} - population {population}"
     return full_country_description.title()
 
 
-# print(get_city_country('lagos', 'nigeria'))
+# print(get_city_country('lagos', 'nigeria', 3000000))
--- chapter11/test_cities.py
+++ chapter11/test_cities.py
@@ -5,8 +5,10 @@ from city_functions import get_city_country
 class CityCountryTest(unittest.TestCase):
 
     def test_city_country(self):
-        formatted_output = get_city_country('santiago', 'chile')
-        self.assertEquals(formatted_output, 'Santiago, Chile')
+        formatted_output = get_city_country(
+            'lagos', 'nigeria', population=3_0000)
+        self.assertEquals(formatted_output,
+                          'Lagos, Nigeria - Population 30000')
 
 
 if __name__ == '__main__':
