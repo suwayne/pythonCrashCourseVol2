cbc9df6531990b77c7bdb80b9475d49163e4504b
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-11-05 05:49:27 +0100

    revision

--- chapter15/scatter_squares.py
+++ chapter15/scatter_squares.py
@@ -1,19 +1,15 @@
 import matplotlib.pyplot as plt
 
-x_values = range(1, 1001)
-y_values = [x**2 for x in x_values]
-
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+ax.scatter(2, 4, s=200)
 
-# set chart title and label axes.
+# set chart title and label axis.
 ax.set_title("Square Numbers", fontsize=24)
-ax.set_xlabel("Square of value", fontsize=14)
-ax.set_ylabel("Value", fontsize=14)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
 
-# set the range of each axis.
-ax.axis([0, 1100, 0, 1100000])
+# set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
 
-# plt.show()
-plt.savefig('squares_plot.png', bbox_inches='tight')
+plt.show()
