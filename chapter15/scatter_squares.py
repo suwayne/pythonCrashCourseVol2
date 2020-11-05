8ad10d027bc939462e90d1f17f4f05a2d40eed23
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-11-05 02:28:26 +0100

    defining custom colors

--- chapter15/scatter_squares.py
+++ chapter15/scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c='red', s=10)
+ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
 
 # set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
