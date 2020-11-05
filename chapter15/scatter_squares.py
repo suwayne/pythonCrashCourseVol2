55b821c7221aa49df1db905fe1a61a5f87b42f90
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-11-05 02:23:49 +0100

    adding color to scatter method

--- chapter15/scatter_squares.py
+++ chapter15/scatter_squares.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=700)
+ax.scatter(x_values, y_values, c='red' s=10)
 
 # set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
-ax.set_xlabel("Value of x", fontsize=14)
-ax.set_ylabel("Value of y", fontsize=14)
+ax.set_xlabel("Square of value", fontsize=14)
+ax.set_ylabel("Value", fontsize=14)
 
-# set size of tick labels.
-ax.tick_params(axis='both', which='major', labelsize=14)
+# set the range of each axis.
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
