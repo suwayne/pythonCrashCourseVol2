8b7c29751f0f68a46c6e8ad487d64c38e74e3a6b
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-11-07 23:47:15 +0100

    and this guy too

--- debug.py
+++ debug.py
@@ -1,22 +1,22 @@
-# import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-# squares = [1, 4, 9, 16, 25]
+# Define data.
+x_values = list(range(5001))
+cubes = [x**3 for x in x_values]
 
-# fig, ax = plt.subplots()
-# ax.plot(squares)
+# Make plot.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, cubes, s=10)
 
-# plt.show()
+# Set chart title and label axes.
+ax.set_title("Cubes", fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Cube of Value', fontsize=14)
 
-# import matplotlib.pyplot as plt
+# Set size of tick labels, and set range for each axis.
+ax.tick_params(axis='both', labelsize=14)
+ax.axis([0, 5100, 0, 5100**3])
 
-# squares = [1, 4, 9, 16, 25]
-
-# fig, ax = plt.subplots()
-# ax.plot(squares, linewidth=3)
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-x = np.arrange(0, 5, 0.1)
-y = np.sin(x)
-plt.plot(x, y)
+# Show plot.
+plt.show()
