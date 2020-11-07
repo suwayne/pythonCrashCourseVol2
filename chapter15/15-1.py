0ce2ce53a94513af3a78f646dbf7f4e1a572aabd
Osasumwen Ogbebor <osasu.ogbebor@gmail.com>
2020-11-07 23:46:06 +0100

    done for tonight

--- chapter15/15-1.py
+++ chapter15/15-1.py
@@ -1,51 +1,53 @@
 # import numpy as np
 # import matplotlib.pyplot as plt
-from matplotlib import pyplot as plt
-
-x_values = range(1, 5001)
-y_values = [x**3 for x in x_values]
-
-plt.style.use('seaborn')
-fig, ax = plt.subplots()
-
-#pylint: disable = E1101
-ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
-#pylint: enable = E1101
-
-# set chart title and label axis.
-ax.set_title("Cube Numbers", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Cube of Value", fontsize=14)
-
-# set size of tick labels.
-ax.tick_params(axis='both', which='major', labelsize=14)
-
-# set the range of each axis.
-ax.axis([0, 1100, 0, 1100000])
-
-plt.show()
-
+# ....................................
 # from matplotlib import pyplot as plt
 
-# # define data.
-# x_values = [1, 2, 3, 4, 5]
-# cubes = [1, 8, 27, 64, 125]
+# x_values = range(1, 5001)
+# y_values = [x**3 for x in x_values]
 
-# # make plot.
 # plt.style.use('seaborn')
-# fix, ax = plt.subplots()
-# ax.scatter(x_values, cubes, edgecolor='none', s=40)
+# fig, ax = plt.subplots()
 
-# # set chart title and label axes.
-# ax.set_title("cubes", fontsize=24)
-# ax.set_xlabel("value", fontsize=14)
-# ax.set_ylabel("cube of value", fontsize=14)
+# #pylint: disable = E1101
+# ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+# #pylint: enable = E1101
+
+# # set chart title and label axis.
+# ax.set_title("Cube Numbers", fontsize=24)
+# ax.set_xlabel("Value", fontsize=14)
+# ax.set_ylabel("Cube of Value", fontsize=14)
 
 # # set size of tick labels.
-# ax.tick_params(axis='both', labelsize=14)
+# ax.tick_params(axis='both', which='major', labelsize=14)
 
-# # set size of tick labels.
-# ax.tick_params(axis='both', labelsize=14)
+# # set the range of each axis.
+# ax.axis([0, 1100, 0, 1100000])
 
-# # show plot.
 # plt.show()
+# ...............................................
+
+from matplotlib import pyplot as plt
+
+# define data.
+x_values = [1, 2, 3, 4, 5]
+cubes = [1, 8, 27, 64, 125]
+
+# make plot.
+plt.style.use('seaborn')
+fix, ax = plt.subplots()
+ax.scatter(x_values, cubes, edgecolor='none', s=40)
+
+# set chart title and label axes.
+ax.set_title("cubes", fontsize=24)
+ax.set_xlabel("value", fontsize=14)
+ax.set_ylabel("cube of value", fontsize=14)
+
+# set size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
+
+# set size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
+
+# show plot.
+plt.show()
---
+++ chapter15/15-2.py
@@ -0,0 +1,25 @@
+from matplotlib import pyplot as plt
+
+# Define data.
+x_values = list(range(5001))
+cubes = [x**3 for x in x_values]
+
+# Make plot.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+
+# #pylint: disable = E1101
+ax.scatter(x_values, cubes, c=cubes, cmap=plt.cm.Greens, s=10)
+# #pylint: enable = E1101
+
+# Set chart title and label axes.
+ax.set_title("Cubes", fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Cube of Value', fontsize=14)
+
+# Set size of tick labels, and set range for each axis.
+ax.tick_params(axis='both', labelsize=14)
+ax.axis([0, 5100, 0, 5100**3])
+
+# Show plot.
+plt.show()
