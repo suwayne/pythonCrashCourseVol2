dfc0683953960ebf0598d858c057dfb069622b9f
suwayne <osasu.ogbebor@gmail.com>
2021-02-04 07:00:00 +0100

    revision on adding titles to x & y axis

--- chapter15/reviews.py
+++ chapter15/reviews.py
@@ -5,5 +5,12 @@ squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
 ax.plot(squares, linewidth =1)
 
-plt.show()
+#set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=7)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=7)
 
+#size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
+
+plt.show()
