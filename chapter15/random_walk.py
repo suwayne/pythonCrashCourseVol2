d027351e73356737ec5d07e4ecd82664fc407f36
suwayne <osasu.ogbebor@gmail.com>
2021-02-07 10:48:48 +0100

    new commit

--- chapter15/random_walk.py
+++ chapter15/random_walk.py
@@ -4,7 +4,7 @@ class RandomWalk:
     """A class to generate random walks"""
 
     def __init__(self, num_points=5000):
-        """initialize attributes of a walk"""
+        """initialize attributes of a walk"""   
         self.num_points = num_points
 
         #All walks start at (0, 0).
--- chapter15/rw_visual.py
+++ chapter15/rw_visual.py
@@ -57,5 +57,24 @@ while True:
     if keep_running == 'n':
         break
 
+"""coloring the points"""
+while True:
+#Make a random walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-    """coloring the points"""
+    #plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    #pylint: disable = #1101
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    #pylint: disable = #1101
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n)")
+    if keep_running == 'n':
+        break
+
+
+
