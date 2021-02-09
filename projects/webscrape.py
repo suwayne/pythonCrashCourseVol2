0d7e48fcc9718eb982a3f75f27629e4bffc5bc0b
suwayne <osasu.ogbebor@gmail.com>
2021-02-09 18:25:16 +0100

    ws reboot

--- projects/webscrape.py
+++ projects/webscrape.py
@@ -13,3 +13,12 @@ page_soup = soup(page_html, "html.parser")
 
 #grabs each product
 containers = page_soup.findAll("div", {"class":"item-container"})
+
+for container in containers:   
+    brand = container.div.div.a.img["title"]
+
+    title_container = container.findAll("a", {"class":"item-title"})
+    product_name = title_container[0].text
+
+    shipping_container = container.findAll("li", {"class":"price-ship"})
+    
