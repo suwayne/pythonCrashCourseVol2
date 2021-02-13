6cc13010210daea6015aeb627952d1d8dd17000d
suwayne <osasu.ogbebor@gmail.com>
2021-02-13 17:29:50 +0100

    new commit

--- chapter16/revision.py
+++ chapter16/revision.py
@@ -1,47 +1,44 @@
 import csv
 from datetime import datetime
-
 import matplotlib.pyplot as plt
 
-filename = '/Users/wayne/Documents/dev/pythonCrashCourseVol2/chapter16/data/sitka_weather_07-2018_simple.csv'
+# filename = '/Users/wayne/Documents/dev/pythonCrashCourseVol2/chapter16/data/sitka_weather_07-2018_simple.csv'
+# with open(filename) as f:
+#     reader = csv.reader(f)
+#     header_row = next(reader)
+#     print(header_row)
 
+#     for index, column_header in enumerate(header_row):
+#         print(index, column_header)
+        
+filename = '/Users/wayne/Documents/dev/pythonCrashCourseVol2/chapter16/data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
 #get dates and high temparatures from this file
     dates, highs = [], []
+#get high temperatures from this file.
+#highs = []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
         dates.append(current_date)
         highs.append(high)
 
-#plot the high temparatures
+# print(highs)
+
+#plot the high temperatures
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 
 #format plot
-plt.title("Daily high temparatures, July 2018", fontsize=24)
+plt.title("Daily high temperatures, July 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temparature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
-
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
