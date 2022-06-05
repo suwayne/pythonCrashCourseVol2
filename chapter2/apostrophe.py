28afd604c00b7f419b3f7e507aa97abff4424cdf
osasu <osasu.ogbebor@gmail.com>
2022-06-06 00:42:54 +0100

    new commit

--- chapter2/apostrophe.py
+++ chapter2/apostrophe.py
@@ -1,2 +1,6 @@
-message = 'One of Python's strengths is its diverse community.'
+#this example shows you why you shouldn't be messing with special characters in strings, 
+#but if you do have them, you can fix that with the back slash. This thing gave me headaches on terminal at some point. 
+#feels good to be able to revert back to old examples for revision.
+
+message = "\'One of Python\'s strengths is its diverse community.\'"
 print(message)
