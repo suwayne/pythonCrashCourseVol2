cf313f544e0bf66fb4808205a29983024fbfe10c
osasu <osasu.ogbebor@gmail.com>
2022-06-06 00:41:57 +0100

    new commit

---
+++ chapter3/revision/bicycles.py
@@ -0,0 +1,34 @@
+bicycles = ['trek', 'cannondale', 'specialized']
+#print(bicycles) here you printed all the items in the list
+#print(bicycles[0]) here you printed the first item on the list
+
+#using fstring and a method with the list.
+#message = f"My first bicycle was a {bicycles[1].title()}." 
+
+#print(message)
+
+
+#ex1 Printing each of your friends names from the list
+# names = ['Nayin', 'Dele', 'Ita', 'chidera']
+
+# # print("Here's a list of your men dem.")
+# # print(names[1])
+# # bestman = f"My bestman for the day will definitely be {names[0]}, but sadly he won\'t make it due to family commitments."
+# # likemind = f"One like minded individual I truely respect is {names[3]}, he\'s an absolute gentleman and a no bs individual."
+
+
+# # print(bestman)
+# # print(likemind)
+
+# #ex2 
+
+# greeting = f"Have a merry christmas and may your days be bright {names[3]}" 
+
+# print(greeting)
+
+#ex3
+transportType = ['motorcycle', 'car', 'truck', 'boat']
+message = f"I would love to own a {transportType[0]}"
+
+
+print("I would love to own a " + transportType[0])
