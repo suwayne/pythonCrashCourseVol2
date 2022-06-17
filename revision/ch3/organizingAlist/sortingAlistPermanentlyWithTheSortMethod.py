bcc341718982079e618efe81f5f7592834798e87
osasu <osasu.ogbebor@gmail.com>
2022-06-17 01:58:39 +0100

    new commit

--- revision/ch3/organizingAlist/sortingAlistPermanentlyWithTheSortMethod.py
+++ revision/ch3/organizingAlist/sortingAlistPermanentlyWithTheSortMethod.py
@@ -1,5 +1,13 @@
+from numpy import true_divide
+
+
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 
 cars.sort() #the sort function will change the order of the list permanently
 print(cars) 
+
+cars.sort(reverse=True)
+print(cars)
+
+
 
---
+++ revision/ch3/organizingAlist/sortingAlistTemporarilyWithSortedFunction.py
@@ -0,0 +1,14 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the orginial list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars)) #here you're sorting the list using the sorted function
+
+
+print("\nHere is the original list again")
+print(cars)
+
+
+
