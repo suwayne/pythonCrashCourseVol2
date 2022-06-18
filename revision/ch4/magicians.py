d39fd633c6cff4af1700ffadd86dd80aa42d650a
osasu <osasu.ogbebor@gmail.com>
2022-06-18 01:28:50 +0100

    new commit

---
+++ revision/ch4/magicians.py
@@ -0,0 +1,12 @@
+magicians = ['alice', 'david', 'carolina']
+
+for magician in magicians: #this for loop tells python to pull a name from the list and assign it to the variable 'magician
+    print(magician)
+
+#because there are more items in the list, after the first item (alice) is printed, python returns to the list to pick another item to print..etc, till 
+#there are no more items to print and it ends.
+
+
+
+
+
---
+++ revision/ch4/magicians2.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+
+for magician in magicians: 
+    print(f"{magician.title()}, that was a great trick")
+    print(f"{magician.title()}, we can't wait to see your next trick")
+
+
