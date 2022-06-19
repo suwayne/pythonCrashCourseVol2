3b6600fae385ab8ea873a7a34464b1a581e3b227
osasu <osasu.ogbebor@gmail.com>
2022-06-19 18:20:17 +0100

    new commit

---
+++ revision/ch4/ex4_5.py
@@ -0,0 +1,14 @@
+#summing a million
+
+amillion = list(range(1, 1000001))
+
+# print(amillion)
+
+
+print(min(amillion))
+
+print(max(amillion))
+
+print(sum(amillion))
+
+
--- revision/ch4/listcomprehension.py
+++ revision/ch4/listcomprehension.py
@@ -1,2 +1,5 @@
-squares = [value ** 2 for value in range(1, 11)]
+squares = [value ** 2 for value in range(1, 100)]
+
 print(squares)
+
+
