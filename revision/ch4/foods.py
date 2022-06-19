787a23af95c4e37db8f8e7cfe5a52edf4a789fb8
osasu <osasu.ogbebor@gmail.com>
2022-06-20 00:38:00 +0100

    new commit

---
+++ revision/ch4/foods.py
@@ -0,0 +1,19 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friendFoods = my_foods[:] #here's how you copy a whole list using empty first and second index.
+
+print("my favorite foods are:")
+print(friendFoods)
+
+
+my_foods.append('canoli')
+friendFoods.append('ice cream')
+print(my_foods)
+
+print(friendFoods)
+
+
+my_foods = friendFoods[:]
+my_foods.append('canoli')
+friendFoods.append('ice cream')
+
+print(my_foods)
