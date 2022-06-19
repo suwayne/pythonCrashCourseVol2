4c51a976ba2d0afe13495323ea4aade684520833
osasu <osasu.ogbebor@gmail.com>
2022-06-19 20:26:19 +0100

    new commit

---
+++ revision/ch4/ex4_6.py
@@ -0,0 +1,15 @@
+"""
+Odd Numbers: Use the third argument of the range() function 
+to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.
+"""
+
+oddnumbers = list(range(1, 21, 2))
+
+for oddnumber in oddnumbers:
+    print(oddnumber)
+
+
+# print(oddnumbers)
+
+
+
