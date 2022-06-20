bd397646487dea8c40fc830f4228179f66cfa70f
osasu <osasu.ogbebor@gmail.com>
2022-06-20 01:58:10 +0100

    new commit

---
+++ revision/ch4/ex4_13.py
@@ -0,0 +1,16 @@
+buffetrestaurant = ('suya', 'fish kebab', 'spring rolls', 'pounded yam', 'prawns')
+
+for buffet in buffetrestaurant:
+    print(buffet)
+
+# buffetrestaurant[0] = 'chicken'
+
+print(buffetrestaurant)
+
+print("\nsee new restaurant menu:")
+buffetrestaurant = ('suya bistro', 'fish kebab', 'fish rolls', 'pounded yam', 'prawns')
+
+for buffet in buffetrestaurant:
+    print(buffet)
+
+
