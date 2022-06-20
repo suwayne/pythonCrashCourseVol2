931ee8a128a962008d1e93c7855738ae9f06b30a
osasu <osasu.ogbebor@gmail.com>
2022-06-20 01:12:01 +0100

    new commit

--- revision/ch4/tuples.py
+++ revision/ch4/tuples.py
@@ -2,3 +2,8 @@ dimensions = (200, 50)
 print(dimensions[1])
 
 
+dimensions[0] = 20 #you cannot change the value of tuple
+
+print(dimensions) #this is proof that you cannot change the value of a tuple
+
+
