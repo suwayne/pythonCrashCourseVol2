8c9f868ea4f7620019c10755342c0115d93a2531
osasu <osasu.ogbebor@gmail.com>
2022-06-20 01:19:39 +0100

    new commit

---
+++ revision/ch4/loopingthroughtuples.py
@@ -0,0 +1,15 @@
+dimensions = (200, 50)
+for dimension in dimensions:
+    print(dimension)
+
+
+#writing over a tuple
+print("\noriginal dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nmodified dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
--- revision/ch4/tuples.py
+++ revision/ch4/tuples.py
@@ -1,9 +1,15 @@
-dimensions = (200, 50)
-print(dimensions[1])
+# dimensions = (200, 50)
+# print(dimensions[1])
 
 
-dimensions[0] = 20 #you cannot change the value of tuple
+# dimensions[0] = 20 #you cannot change the value of tuple
 
-print(dimensions) #this is proof that you cannot change the value of a tuple
+# print(dimensions) #this is proof that you cannot change the value of a tuple
+
+
+age = (34,)
+
+print(age[0])
+
 
 
