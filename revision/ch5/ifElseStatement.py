7bf78b3a14039e2a388ad666abbcec1b9332db00
osasu <osasu.ogbebor@gmail.com>
2022-06-26 23:37:00 +0100

    new commit

---
+++ revision/ch5/ifElseStatement.py
@@ -0,0 +1,9 @@
+age = 17
+if age >= 18:
+    print("you are old enough to votte")
+    print("have you registered to vote yet")
+else:
+    print("sorry, you are too young to vote")
+    print("please register to vote as soon as you turn 18!")
+
+    
---
+++ revision/ch5/voting.py
@@ -0,0 +1,7 @@
+age = 19
+if age >= 18:
+    print("you are old enough to vote!")
+    print("have you registered to vote yet!")
+
+    
+
