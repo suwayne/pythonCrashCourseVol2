c3b54a270633f8f80170c6b7f890c769be9e6e33
osasu <osasu.ogbebor@gmail.com>
2022-06-29 01:09:42 +0100

    new commit

---
+++ revision/ch5/aliencolors2_5_4.py
@@ -0,0 +1,16 @@
+alien_color = 'yellow'
+
+# if alien_color == 'green':
+#     print("you just earned 5 points") 
+
+# if alien_color != 'green':
+#     print("you just earned 10 points")
+
+
+#version with elif block
+
+if alien_color == 'green':
+    print("you just earned 5 points") 
+
+elif alien_color != 'green':
+    print("you just earned 10 points")
---
+++ revision/ch5/ex5_5.py
@@ -0,0 +1,16 @@
+alien_color = 'yellow'
+
+#version with elif block
+
+if alien_color == 'green':
+    value = 5
+elif alien_color == 'yellow':
+    value = 10
+elif alien_color == 'red':
+    value = 15
+
+print(f"you just earned {value} points.")
+
+
+
+
