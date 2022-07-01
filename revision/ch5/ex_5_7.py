6c59053bcebc1d443bc047883095a0868d1aa7be
osasu <osasu.ogbebor@gmail.com>
2022-07-01 18:26:32 +0100

    new commit

---
+++ revision/ch5/ex_5_7.py
@@ -0,0 +1,9 @@
+favorite_fruits = ["mango", "apple", "pineapple", "banana", "orange"]
+
+if "banana" in favorite_fruits:
+    print(f"you truly love bananas")
+
+if "mango" in favorite_fruits:
+    print(f"you truly love {favorite_fruits[0]}s") 
+
+    
