9c35ae45b86e99a93be87443f8105c62278dd268
osasu <osasu.ogbebor@gmail.com>
2022-07-01 18:48:19 +0100

    new commit

--- revision/ch5/toppings.py
+++ revision/ch5/toppings.py
@@ -1,11 +1,18 @@
-requested_toppings = ['mushrooms', 'extra cheese']
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 
-if 'mushrooms' in requested_toppings: 
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings: 
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_toppings: 
-    print("Adding extra cheese.")
+# if 'mushrooms' in requested_toppings: 
+#     print("Adding mushrooms.")
+# if 'pepperoni' in requested_toppings: 
+#     print("Adding pepperoni.")
+# if 'extra cheese' in requested_toppings: 
+#     print("Adding extra cheese.")
                  
                  
-print("\nFinished making your pizza!")
+#making the code more efficient. 
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}.")
+
+print("\nfinished making your pizza!")
+
+
+
