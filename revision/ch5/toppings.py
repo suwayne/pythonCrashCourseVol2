219962ea9dfe034d4e973a1dafc018e74d605ab1
osasu <osasu.ogbebor@gmail.com>
2022-07-02 23:27:37 +0100

    new commit

--- revision/ch5/toppings.py
+++ revision/ch5/toppings.py
@@ -9,10 +9,27 @@ requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
                  
                  
 #making the code more efficient. 
+# for requested_topping in requested_toppings:
+#     print(f"Adding {requested_topping}.")
+
+# print("\nfinished making your pizza!")
+
+
+#what if pizzeria runs out of green peppers?
+
 for requested_topping in requested_toppings:
-    print(f"Adding {requested_topping}.")
+    if requested_toppings == "green peppers":
+        print("sorry, we are out of peppers right now.")
+    else:
+        print(f"adding {requested_topping}.")
+
 
 print("\nfinished making your pizza!")
+
+
+
+
+
 
 
 
