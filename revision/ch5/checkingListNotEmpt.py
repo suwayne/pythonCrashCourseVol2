0ec4c998ff1e5ba48dc81909be2b85ca391d9f11
osasu <osasu.ogbebor@gmail.com>
2022-07-03 00:22:23 +0100

    new commit

--- revision/ch5/checkingListNotEmpt.py
+++ revision/ch5/checkingListNotEmpt.py
@@ -1,11 +1,10 @@
-from urllib import request
+#i've tried this out with an empty list and non-empty list.
 
-
-requested_toppings = []
+requested_toppings = ["taco bell", "sharwama", "saussage"]
 
 if requested_toppings:
     for requested_topping in requested_toppings:
-        print(f"Adding{requested_topping}")
+        print(f"Adding {requested_topping}")
     print("\nfinished making your pizza!")
 
 else: 
