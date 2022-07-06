324d09ac79c7cea82f69d7ed8d9b9b018d07c150
osasu <osasu.ogbebor@gmail.com>
2022-07-06 02:31:50 +0100

    new commit

--- revision/ch5/checkingListNotEmpt.py
+++ revision/ch5/checkingListNotEmpt.py
@@ -4,11 +4,11 @@
 
 requested_toppings = []
 
-if requested_toppings: #this part evaluates to true or false
+if requested_toppings:                           #this part evaluates to true or false
     for requested_topping in requested_toppings: #this part executes if it's true
         print(f"Adding {requested_topping}")
     print("\nfinished making your pizza!")
 
-else: #this part executes if it's false
+else:                                            #this part executes if it's false
     print("are you sure you want a plain pizza?")
 
