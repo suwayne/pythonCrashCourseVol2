1ff895d93b3cbc4d1228b84073cdd566e84fb615
osasu <osasu.ogbebor@gmail.com>
2022-07-06 22:59:19 +0100

    new commit

---
+++ revision/ch6/asimpledictionary.py
@@ -0,0 +1,10 @@
+#with dictionaries you can store values into a variable using 'key / value' pairs. pretty easy :). And you can have an ulimited
+#number of pairs
+
+#alien.py
+
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
