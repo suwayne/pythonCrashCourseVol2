38460c83e9e1460dddd8f6d730deb94fce8c8c84
osasu <osasu.ogbebor@gmail.com>
2022-07-06 15:35:50 +0100

    new commit

--- revision/ch4/squares.py
+++ revision/ch4/squares.py
@@ -10,3 +10,7 @@ for square in range(1, 11):
 print(squares)
 
 
+squares = [value ** 2 for value in range(1, 11)]
+
+print(squares)
+
--- revision/ch5/ex5_10.py
+++ revision/ch5/ex5_10.py
@@ -1,22 +1,23 @@
-current_users = ["iyayi", "osita", "morakinyo", "joshua", "ghome"]
-new_users = ["iyayi", "joshua", "dami", "sagir", "chidera"]
+# current_users = ["iyayi", "osita", "morakinyo", "home lander", "ghome"]
+# new_users = ["iyayi", "joshua", "dami", "sagir", "chidera"]
 
-for new_user in new_users:
-    if new_user in current_users:
-        print(f"the username {new_user} is available.")
-    else:
-        print("you will need to enter a new username.")
+# for new_user in new_users:
+#     if new_user in current_users:
+#         print(f"the username {new_user} is available.")
+#     else:
+#         print("you will need to enter a new username.")
         
 
 ######## here's the solution i found on reddit.
 
-# current_users = ['eric', 'willie', 'admin', 'erin', 'Ever']
-# new_users = ['sarah', 'Willie', 'PHIL', 'ever', 'Iona']
+current_users = ['eric', 'willie', 'admin', 'erin', 'Ever']
+new_users = ['sarah', 'Willie', 'PHIL', 'ever', 'Iona']
 
-# current_users_lower = [user.lower() for user in current_users]
+current_users_lower = [user.lower() for user in current_users]
 
-# for new_user in new_users:
-#     if new_user.lower() in current_users_lower:
-#         print(f"Sorry {new_user}, that name is taken.")
-#     else:
-#         print(f"Great, {new_user} is still available.")
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print(f"Sorry {new_user}, that name is taken.")
+    else:
+        print(f"Great, {new_user} is still available.")
+
