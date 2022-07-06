cf86c309b7ee5f5394b948d53e9a458f682a146c
osasu <osasu.ogbebor@gmail.com>
2022-07-06 22:42:52 +0100

    new commit

---
+++ revision/ch5/ex5_11.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in numbers: 
+    if number == 1:
+        print(f"this is the {number}st digit")
+    elif number == 2:
+        print(f"this is the {number}nd digit")
+    elif number ==3:
+        print(f"this is the {number}rd digit")
+    
+    else:
+        print(f"this is the {number}th digit")
+
+
+
+
