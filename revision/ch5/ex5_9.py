506a5017305eba9d7b90b5c7f71ef776c795f5ab
osasu <osasu.ogbebor@gmail.com>
2022-07-06 02:48:57 +0100

    i understand how to check if a list is not empty

--- revision/ch5/ex5_9.py
+++ revision/ch5/ex5_9.py
@@ -8,10 +8,13 @@ usernames = []
 
 if usernames:
     for username in usernames:
-        if username == 'admin':
-            print("Hello admin, would you like to see a status report?")
+        if username == "admin":
+            print("hello Admin, would you like to see a status report?")
         else:
-            print(f"Hello {username}, thank you for loggin in again!")
+            print("hello Jaden, thank you for loggin in again.")
+
 else:
-    print("We need to find some users!")
+    print("hello Jaden, thank you for loggin in again.")
+
+
 
