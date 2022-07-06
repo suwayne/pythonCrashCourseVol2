2b2f940972fd60a44b9b073324cea8243ecc0d5b
osasu <osasu.ogbebor@gmail.com>
2022-07-06 23:08:06 +0100

    new commit

--- revision/ch6/asimpledictionary.py
+++ revision/ch6/asimpledictionary.py
@@ -3,8 +3,20 @@
 
 #alien.py
 
+from turtle import color
+
+
 alien_0 = {'color': 'green', 'points': 5}
 
 print(alien_0['color'])
 print(alien_0['points'])
+
+#another example using the dictionary created above:
+
+# new_points = alien_0['points']
+# print(f"you just earned {new_points}.")
+
+# new_color = alien_0['color']
+# print(f"you just took down the {new_color} alien.")
+
 
