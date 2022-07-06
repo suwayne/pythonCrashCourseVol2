6ee50c76a66ed7bcf48d9183d9bc74efef562f72
osasu <osasu.ogbebor@gmail.com>
2022-07-06 23:23:44 +0100

    new commit

---
+++ revision/ch6/addingnewkeypairs.py
@@ -0,0 +1,10 @@
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0)
+
+#adding new key pairs to the alien dictionary
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
