47f1e6dfdddd643b76b79f5d7f8d44178b22a803
osasu <osasu.ogbebor@gmail.com>
2022-07-07 23:45:33 +0100

    new commit

--- randoms.py
+++ randoms.py
@@ -1,13 +1,21 @@
-#fizzbuzz challenge.
+# #fizzbuzz challenge.
 
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print(f"{number} is a multiple of 3 and 5")
-    elif number % 5 == 0:
-        print(f"{number} is a multiple of 5")
-    elif number % 3 == 0:
-        print(f"{number} is a multiple of 3")
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print(f"{number} is a multiple of 3 and 5")
+#     elif number % 5 == 0:
+#         print(f"{number} is a multiple of 5")
+#     elif number % 3 == 0:
+#         print(f"{number} is a multiple of 3")
 
-    else:
-        print(number)
+#     else:
+#         print(number)
   
+
+length = input("input length: ")
+width = input("input width: ")
+
+area = int(length) * int(width)
+
+print(f"the area is: {area}")
+
