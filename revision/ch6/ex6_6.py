1ff7bc2eaac7c1a78b7e2683dd4e94345fa5c12f
osasu <osasu.ogbebor@gmail.com>
2022-07-10 00:53:47 +0100

    new commit

--- revision/ch6/ex6_6.py
+++ revision/ch6/ex6_6.py
@@ -1,6 +1,8 @@
-# Loop through the list of people who should take the poll. If they have already taken the poll, 
-# print a message thanking them for responding. If they have not yet taken the poll, print a message 
-# inviting them to take the poll.
+"""
+Loop through the list of people who should take the poll. If they have already taken the poll, 
+print a message thanking them for responding. If they have not yet taken the poll, print a message 
+inviting them to take the poll.
+"""
 
 favorite_languages = {
        'jen': 'python',
@@ -9,15 +11,24 @@ favorite_languages = {
        'phil': 'python',
        }
 
-#making a list of people who should have taken the poll.
+
+# making a list of people who should have taken the poll.
 pollers =  ['victor', 'edward', 'james', 'phil', 'percy']
 
+for name in pollers:
+    if name in favorite_languages.keys():
+        print(f"thank you {name.title()} for responding to the poll.")
+    else:
+        print(f"hi, {name.title()} don't forget to take the poll.")
+   
+
+
 #looking up the favorite language of one of the people that took the poll:
-language = favorite_languages['edward'].title()
+# language = favorite_languages['edward'].title()
 
 # print(language)
 
-for name, language in favorite_languages.items():
-    print(f"{name.title()}s favorite programming language is: {language.title()}.")
+# for name, language in favorite_languages.items():
+#     print(f"{name.title()}s favorite programming language is: {language.title()}.")
 
 
