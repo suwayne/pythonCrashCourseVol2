b0d674239c6e4a49d50482fa47de21d16ce35223
osasu <osasu.ogbebor@gmail.com>
2022-07-09 14:30:12 +0100

    new commit

---
+++ revision/ch6/ex6_3.py
@@ -0,0 +1,14 @@
+#glossary
+glossary = {
+    'data types': 'examples are integers, strings, lists, etc.',
+    'if statements': 'helps you work with multiple conditions in your program',
+    'f strings': 'use this with print statements to include store values in variables with your output',
+    'type function': 'insert any datatype in this type() function to find out more about it',
+    'dir function': 'displays details about any object you put in it',
+}
+
+for item in glossary:
+    print(item, ':\n', glossary[item], '\n')
+
+
+
