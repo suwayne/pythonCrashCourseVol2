2c25cabc08766f42b9289f5c7d9641aa81b6fd15
osasu <osasu.ogbebor@gmail.com>
2022-07-10 08:01:07 +0100

    new commit

---
+++ revision/ch6/nesting.py
@@ -0,0 +1,11 @@
+#nesting dictionaries in a list
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+    
