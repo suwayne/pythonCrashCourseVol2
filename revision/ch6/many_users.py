6a75f3e133d29c5a6c9dd8ce860a9a976a7f5927
osasu <osasu.ogbebor@gmail.com>
2022-07-16 06:47:56 +0100

    new commit

--- revision/ch6/many_users.py
+++ revision/ch6/many_users.py
@@ -16,6 +16,6 @@ for username, user_info in users.items():
     print(f"\nUsername: {username}")
     full_name = f"{user_info['first']} {user_info['last']}"
     location = user_info['location']
-    
+
     print(f"\tFull name: {full_name.title()}")
     print(f"\tLocation: {location.title()}")
