3d5f081a0bce0558cf1afeb8b16761565b8ca5ae
osasu <osasu.ogbebor@gmail.com>
2022-07-17 01:14:28 +0100

    new commit

---
+++ revision/ch6/ex6_7people.py
@@ -0,0 +1,12 @@
+iyayi_vivian = {
+    'first name': 'iyayi',
+    'last name': 'ogbebor',
+    'city': 'waltham',
+}
+
+osasu_ogbebor = {
+    'first name': 'osasu', 
+    'last name': 'ogbebor', 
+    'city': 'lagos',
+}
+
