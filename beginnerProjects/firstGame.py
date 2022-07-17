d9de837157b18d919b17395f10ba218c21b0a75b
osasu <osasu.ogbebor@gmail.com>
2022-07-17 01:15:44 +0100

    new commit

--- beginnerProjects/firstGame.py
+++ beginnerProjects/firstGame.py
@@ -1,6 +1,6 @@
 print("welcome to my first game!")
 name = input("what is your name? ")
-age = input("what is your age? ")
+age = int(input("what is your age? "))
 
 print("hello", name, "you are", age, "years old.")
 
