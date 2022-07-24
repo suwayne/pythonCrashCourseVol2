f618ca81a75644e0482d9565e8d2e69567a32a72
osasu <osasu.ogbebor@gmail.com>
2022-07-24 13:47:51 +0100

    new commit

--- revision/ch6/ex6_9.py
+++ revision/ch6/ex6_9.py
@@ -9,9 +9,9 @@ favorite_places = {
 
 }
 print("Exercise 6_9.py: Favorite places:")
-for name, places in favorite_places.items():
-    print(f"{name} loves to visit the following places:")
-    for place in places:
+for name, places in favorite_places.items():        #you decleared a key value pair for the dictionary here
+    print(f"{name} loves to visit the following places:")       #printing the keys in the dictionary at this point
+    for place in places:        #this line loops through every 'value' in the dictionary, list mode :)
         print(f"- {place.title()}")
 
 
