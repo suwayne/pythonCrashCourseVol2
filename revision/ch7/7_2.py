efd13bcf2e59d8c9805521b867a43a01fa896910
osasu <osasu.ogbebor@gmail.com>
2022-07-24 19:30:13 +0100

    new commit

--- revision/ch7/7_2.py
+++ revision/ch7/7_2.py
@@ -3,7 +3,7 @@ from tokenize import group
 
 
 groupSize = input("Hello customer, how many people are in your dinner group?: ")
-if groupSize > 8:
+if int(groupSize) > 8:
     print(f"Hello customer, sadly, you'll have to wait for anothe table. a group of {groupSize}")
     print(f"is larger than 8")
 else:
--- revision/ch7/ex7_1.py
+++ revision/ch7/ex7_1.py
@@ -1,3 +1,4 @@
 #rental car
 carType = input("what kind of rental car would you like?: ")
 print(f"Thank you for your feedback. Let me see if I can find you a {carType}.")
+
