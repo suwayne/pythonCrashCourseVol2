57dce277bfe482718b032f03e676fcdafa65fe18
osasu <osasu.ogbebor@gmail.com>
2022-07-24 19:12:16 +0100

    new commit

---
+++ revision/ch7/ex7_1.py
@@ -0,0 +1,3 @@
+#rental car
+carType = input("what kind of rental car would you like?: ")
+print(f"Thank you for your feedback. Let me find you a {carType}.")
--- revision/ch7/moduloOperator.py
+++ revision/ch7/moduloOperator.py
@@ -2,3 +2,12 @@
 the % 'modulo' operator tells you how much is left after you divide two numbers,
 it's also a good way to determine whether a number is even or odd.
 """
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print(f"The value: {number} you entered is even.")
+else:
+    print(f"The value: {number} you entered is odd.")
+
+
