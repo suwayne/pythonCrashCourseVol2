c05524e6991791f6900ea24e88c97b943a6a1132
osasu <osasu.ogbebor@gmail.com>
2022-07-24 19:50:04 +0100

    new commit

---
+++ revision/ch7/multipleof7s.py
@@ -0,0 +1,8 @@
+number =input("Hi user, please enter a number: ")
+number = int(number)
+
+if number % 10 == 0:
+    print(f"The {number} is a multiple of 10")
+else:
+    print(f"The {number} is not a multiple of 10")
+
