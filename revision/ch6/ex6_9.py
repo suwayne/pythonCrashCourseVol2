cb0be400d6c33b2d2eaae3de63557c729cfb1afc
osasu <osasu.ogbebor@gmail.com>
2022-07-24 13:45:05 +0100

    new commit

--- revision/ch6/ex6_9.py
+++ revision/ch6/ex6_9.py
@@ -4,12 +4,15 @@ favorite places exercise
 
 favorite_places = {
     'iyayi': ['miami', 'tunisia', 'france'],
-    'osasu': ['abu dhabi', 'seychelles',],
-    'nayin': ['texas', 'massachusettes', 'morocco'],
+    'osasu': ['abu dhabi', 'seychelles','argentina'],
+    'mosope': ['texas', 'massachusettes', 'morocco'],
 
 }
 print("Exercise 6_9.py: Favorite places:")
-for name, place in favorite_places.items():
-    print(f"{name}'s favorite places to visit are: {place}")
+for name, places in favorite_places.items():
+    print(f"{name} loves to visit the following places:")
+    for place in places:
+        print(f"- {place.title()}")
+
 
 
