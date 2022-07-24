e373665b664b4a63eb623215f89e51d649c3dc3d
osasu <osasu.ogbebor@gmail.com>
2022-07-24 14:00:34 +0100

    new commit

---
+++ revision/ch6/ex6_10.py
@@ -0,0 +1,19 @@
+#favorite numbers:
+
+favorite_numbers = {
+    'osasu': [9, 43, 20, 12],
+    'elijah': [12, 42, 11, 42],
+    'joshua': [4, 9, 35, 84],
+    'esosa': [9, 23, 95, 32],
+}
+
+#print each persons name and number.
+# for number in favorite_numbers:
+#     print(number, '>', favorite_numbers[number])
+
+for key, value in favorite_numbers.items():
+    # print(key,':', value)
+    print(f"key: {key}'s favorite numbers:")
+    for val in value:
+        print(f"- {val}")
+
