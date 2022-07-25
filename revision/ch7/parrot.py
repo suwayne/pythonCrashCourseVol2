9fbd7da2957f46332f911b2e60f98408d582d1fc
osasu <osasu.ogbebor@gmail.com>
2022-07-25 02:28:29 +0100

    new commit

--- revision/ch7/parrot.py
+++ revision/ch7/parrot.py
@@ -1,8 +1,8 @@
 prompt = "\nTell me something, and I will repeat it back to you:" 
 prompt += "\nEnter 'quit' to end the program.\n: "
 
+
 message = ""
-while message.lower() != 'quit':
+while message != "quit":
     message = input(prompt)
     print(message)
-
