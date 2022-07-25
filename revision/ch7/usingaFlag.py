813740e87635dde9cf27a2d10b0d47b9a41681a1
osasu <osasu.ogbebor@gmail.com>
2022-07-25 02:44:42 +0100

    review using flags for more complex code

---
+++ revision/ch7/usingaFlag.py
@@ -0,0 +1,15 @@
+#using a flag
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == "quit":
+        active = False
+    else: 
+        print(message)
+    
+
+   
