f4808742d7845857a4f4c2706a79ad66a8509d4e
osasu <osasu.ogbebor@gmail.com>
2022-07-28 05:30:41 +0100

    new commit

--- revision/ch7/ex7_5.py
+++ revision/ch7/ex7_5.py
@@ -1,5 +1,19 @@
 #movie tickets
 """
 A loop in which you ask users their age, and then tell them the cost of their movie ticket.
+
+A movie theater charges different ticket prices depending on a persons age. If a person is under the age of 3, 
+the ticket is free; if they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15. 
+Write a loop in which you ask users their age, and then tell them the cost of their movie ticket.
 """
+
+prompt = "\nHow old are you? "
+prompt += "\nEnter quit when you're finished: "
+
+while True:
+    age = input(prompt)
+    if age == 'quit':
+        print("You have ended this program")
+        break
+    age = int(age)
 
