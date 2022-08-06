b2bd23cb6381e6e3e866eea1157a82a514b5eaa4
osasu <osasu.ogbebor@gmail.com>
2022-08-06 05:44:08 +0100

    new commit

---
+++ revision/ch8/ex8_1.py
@@ -0,0 +1,10 @@
+#ex message
+"""Write a function called display_message() that prints one sen- tence telling everyone what you are learning about in this chapter. 
+Call the function, and make sure the message displays correctly.
+"""
+
+def display_message():
+    """this function prints out what you've learnt in this chapter"""
+    print("Your learning about functions, and this is chapter 8")
+
+display_message()
--- revision/ch8/passinginfotofunc.py
+++ revision/ch8/passinginfotofunc.py
@@ -1,12 +1,14 @@
 """
 Modified slightly, the function greet_user() can not only tell the user Hello! but also greet them by name. 
-For the function to do this, you enter username in the parentheses of the function’s definition at def greet_user(). 
+For the function to do this, you enter username in the parentheses of the functios definition at def greet_user(). 
 By add- ing username here you allow the function to accept any value of username you specify. The function now expects 
 you to provide a value for username each time you call it. When you call greet_user(), you can pass it a name, such as 
 'jesse', inside the parentheses:
 """
-
-def greet_user(username):
+#this example shows how to pass information into a function:
+#parameter: piece of information a function needs to do its job
+#artument: the string 'jesse' is an argument. A piece of information that's passed from a function "greet user" to call the function "greet user"
+def greet_user(username):       #the variable 'usename' is a parameter
     """Display a simple greeting."""
     print(f"Hello, {username.title()}!")
 
