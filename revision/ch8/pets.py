eef3745e4448c85423a654863b2f1d55ad44cfd8
osasu <osasu.ogbebor@gmail.com>
2022-08-06 06:05:59 +0100

    new commit

--- revision/ch8/pets.py
+++ revision/ch8/pets.py
@@ -1,7 +1,7 @@
-def describe_pet(animal_type, pet_name): 
+def describe_pet(animal_type, pet_name): #parameters are in this parenthesis
     """Display information about a pet."""
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-describe_pet('hamster', 'harry')
+describe_pet('hamster', 'harry') #arguments are this parenthesis
 
