1bf7b4a10162baa70301bd76554db9ecd68f58a6
osasu <osasu.ogbebor@gmail.com>
2022-08-07 12:02:05 +0100

    you understand functions with optional parameters

--- revision/ch8/formatted_name.py
+++ revision/ch8/formatted_name.py
@@ -1,13 +1,35 @@
 
-def get_formatted_name(first_name, last_name):
-    """Return full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
-    return full_name.title()    #return will break you out of the function; a print statement wont work after you use return.
+# def get_formatted_name(first_name, last_name):
+#     """Return full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()    #return will break you out of the function; a print statement wont work after you use return.
 
 
 
-musician = get_formatted_name('jimi', 'hendrix')
-print(musician)
+# musician = get_formatted_name('jimi', 'hendrix')
+# print(musician)
 
 
 
+# #function defined with 3 parameters
+# def get_formatted_name(first_name, middle_name, last_name):
+#        """Return a full name, neatly formatted."""
+#        full_name = f"{first_name} {middle_name} {last_name}"
+#        return full_name.title()
+
+
+
+# musician = get_formatted_name('john', 'lee', 'hooker')
+# print(musician)
+
+
+#function defined with optional parameter -middle_name
+def get_formatted_name(first_name, last_name,  middle_name=''):
+       """Return a full name, neatly formatted."""
+       full_name = f"{first_name} {middle_name} {last_name}"
+       return full_name.title()
+
+
+# musician = get_formatted_name('jimi', 'hendrix')
+musician = get_formatted_name('jimi', 'hendrix', 'swagger')
+print(musician) 
