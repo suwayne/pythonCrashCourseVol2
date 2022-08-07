f5dd707300298925cdccc69b63d609168d9cfe9d
osasu <osasu.ogbebor@gmail.com>
2022-08-07 01:37:37 +0100

    new commit

---
+++ revision/ch8/formatted_name.py
@@ -0,0 +1,15 @@
+from numpy import full
+
+
+def get_formatted_name(first_name, last_name):
+    """Return full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()    #return will break you out of the function; a print statement wont work after you use return.
+
+
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+
+
