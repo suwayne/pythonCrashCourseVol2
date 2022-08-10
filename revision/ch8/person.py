acd1c61181eb9dad181834fad98ba94be8169e73
osasu <osasu.ogbebor@gmail.com>
2022-08-10 01:45:59 +0100

    returning a dictionary

--- revision/ch8/person.py
+++ revision/ch8/person.py
@@ -1,7 +1,10 @@
+
+
 def build_person(first_name, last_name):
     """Return a dictionary of information about a person."""
-    person = {'first': first_name, 'last': last_name} 
+    person = {'first':first_name, 'last': last_name}
     return person
 
-musician = build_person('jimi', 'hendrix') 
+musician = build_person('jimi', 'hendrix')
 print(musician)
+
