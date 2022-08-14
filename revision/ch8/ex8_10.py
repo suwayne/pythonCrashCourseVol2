0bc5444af2b474c96f1bfe87afc5fd8c0b5af321
osasu <osasu.ogbebor@gmail.com>
2022-08-15 00:42:06 +0100

    i've got to review chapter8

---
+++ revision/ch8/ex8_10.py
@@ -0,0 +1,7 @@
+"""
+Start with a copy of your program from Exercise 8-9. Write a function called 
+send_messages() that prints each text message and moves each message to a new 
+list called sent_messages as its printed. After calling the function, print 
+both of your lists to make sure the messages were moved correctly.
+"""
+
