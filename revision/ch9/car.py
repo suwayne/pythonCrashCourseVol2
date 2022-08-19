ce6045b04c2be3c1c3341799244af998c752e335
osasu <osasu.ogbebor@gmail.com>
2022-08-19 09:06:56 +0100

    new commit

--- revision/ch9/car.py
+++ revision/ch9/car.py
@@ -37,17 +37,9 @@ class Car:
 
 
 my_new_car = Car('audi', 'a4', 2019)
+my_new_car.update_odometer(3)
+my_new_car.read_odometer()
+my_new_car.update_odometer(1)
+my_new_car.read_odometer()
 
-# print(my_new_car.get_descriptive_name())
-# my_new_car.read_odometer()
 
-# #modifying an attributes value directly
-# my_new_car.odometer_reading = 23
-# my_new_car.read_odometer()
-
-my_used_car = Car('subaru', 'outback', 2015) 
-# print(my_used_car.get_descriptive_name())
-# my_used_car.update_odometer(23_500) 
-# my_used_car.read_odometer()
-# my_used_car.increment_odometer(100) 
-# my_used_car.read_odometer()
