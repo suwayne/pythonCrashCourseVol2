bc037fda0160c71f0ccbd9cd10a88216c5c927b1
osasu <osasu.ogbebor@gmail.com>
2022-09-01 19:52:36 +0100

    new commit

--- revision/ch9/test.py
+++ revision/ch9/test.py
@@ -1,40 +1,12 @@
-class User:
-    def __init__(self, first_name, last_name, sex, country, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.sex = sex
-        self.country = country
-        self.age = age
-        self.login_attempts = 0
-    
-    def describe_user(self):
-        print(f"Hello {self.first_name.title()} {self.last_name.title()} we know you are a {self.age} {self.sex} living in {self.country}")
+"""You are given a string . 
+Your task is to print all possible combinations, up to size , of the string in lexicographic sorted order.
+Input Format
+A single line containing the string  and integer value  separated by a space.
+Constraints
+ 
+The string contains only UPPERCASE characters.
+Output Format
+Print the different combinations of string  on separate lines.
+Sample Input
+HACK 2"""
 
-    def greet_user(self):
-        print(f"Hello {self.first_name.title()} {self.last_name.title()}, welcome onboard")
-
-    def increment_login_attempts(self):
-        self.login_attempts += 1
-    
-    def reset_login_attempts(self):
-        self.login_attempts = 0
-
-
-    
-user1 = User('osasu', 'ogbebor', 'male', 'nigeria', '34')
-
-user1.describe_user()
-user1.greet_user()
-
-iyayi = User('iyayi', 'ogbebor', 'female', 'usa', 29)
-
-iyayi.describe_user()
-iyayi.greet_user()
-
-print(f"number of login attempts by yayi: " + str(iyayi.login_attempts))
-
-iyayi.increment_login_attempts()
-print(f"number of login attempts by yayi: " + str(iyayi.login_attempts))
-
-iyayi.reset_login_attempts()
-print(f"number of login attempts by yayi: " + str(iyayi.login_attempts))
