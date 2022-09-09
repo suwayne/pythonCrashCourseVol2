74d4e497eba96c8e7f5dc410b38274a8971064c4
osasu <osasu.ogbebor@gmail.com>
2022-09-09 21:38:30 +0100

    new commit

--- revision/ch9/test.py
+++ revision/ch9/test.py
@@ -1,14 +1,3 @@
-# #python>introduction>loops
-# def loop(n=30):
-#     # n = int(input('enter a number: '))      
-#     for i in range(1, n+1):
-#         print(i, end = ',')
 
-# # loop(4)
-# loop()
 
-def squares():
-    squares = [value ** 2 for value in range(1, 21)]
-    return squares
 
-print(squares())
