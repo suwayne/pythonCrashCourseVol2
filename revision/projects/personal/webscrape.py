006cce56a20776b11669e1344000aba3d32fd75a
osasu <osasu.ogbebor@gmail.com>
2022-09-25 00:26:36 +0100

    new commit

---
+++ revision/projects/personal/webscrape.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
+data = requests.get(standings_url)
+soup = BeautifulSoup(data.text)
+standings_table = soup.select("table.stats_table")[0]
+
+links = standings_table.find_all("a")
+
+
+# print(stadings_table)
