94a2768547264489e141e39f757ea3beedc6f535
osasu <osasu.ogbebor@gmail.com>
2022-09-25 21:44:41 +0100

    new commit

--- revision/projects/data_visualization/mpl_squares.py
+++ revision/projects/data_visualization/mpl_squares.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 #subplot() generates one or more plots in the same figure
 #fig represents the collection  of plts generated
 #ax represents the single plt in the figure
+plt.style.use('seaborn')
 fig, ax = plt.subplots() 
-ax.plot(squares, linewidth=1)
+# ax.plot(squares, linewidth=1)
+ax.plot(input_values, squares, linewidth=1)
 
 #set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
---
+++ revision/projects/personal/scatter_squares.py
@@ -0,0 +1,16 @@
+"""this is how you plot a single point"""
+
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(2, 4, s=100)
+
+# Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
+plt.show()
+
