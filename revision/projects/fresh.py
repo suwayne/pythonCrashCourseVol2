c901cdd13a42e8d503e46e80180aa93961926a82
osasu <osasu.ogbebor@gmail.com>
2022-10-02 00:43:03 +0100

    new commit

---
+++ revision/projects/fresh.py
@@ -0,0 +1,11 @@
+# from test import favorite_book as fb
+
+# book = fb('the bible')
+# print(book)
+
+
+import test
+
+test.favorite_book('men and gods')
+
+
---
+++ revision/projects/test.py
@@ -0,0 +1,6 @@
+print()
+def favorite_book(title):
+    """Display announcement"""
+    print(f"{title.title()} is my favorite book.")
+
+
