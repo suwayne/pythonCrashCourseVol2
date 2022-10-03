d86169b33c26644ce353f8efbfe395e2cef7cc70
osasu <osasu.ogbebor@gmail.com>
2022-10-03 02:28:50 +0100

    new commit

--- revision/projects/ch15/data_visualization/ex15_1b.py
+++ revision/projects/ch15/data_visualization/ex15_1b.py
@@ -1,14 +1,14 @@
 """
-A number raised to the third power is a cube. Plot the first five cubic numbers.
+Plot the first 5000 cubic numbers.
 """
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-output_values = [1, 8, 27, 64, 125]
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, output_values, linewidth=1)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=7)
 
 #set chart title and label axes.
 ax.set_title("Cube Numbers", fontsize=24)
@@ -19,3 +19,4 @@ ax.set_ylabel("Cube of Value", fontsize=14)
 ax.tick_params(axis="both", labelsize=14)
 
 plt.show()
+
--- revision/projects/test.py
+++
@@ -1,6 +0,0 @@
-print()
-def favorite_book(title):
-    """Display announcement"""
-    print(f"{title.title()} is my favorite book.")
-
-
