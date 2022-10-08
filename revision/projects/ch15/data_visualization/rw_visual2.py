2577fe0cd0fc7a60759562965932d20a9cb4bc80
osasu <osasu.ogbebor@gmail.com>
2022-10-08 01:41:06 +0100

    new commit

--- revision/projects/ch15/data_visualization/rw_visual2.py
+++ revision/projects/ch15/data_visualization/rw_visual2.py
@@ -6,12 +6,12 @@ from random_walk import RandomWalk
 #keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk(70_000)
+    rw = RandomWalk(90_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=100)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
