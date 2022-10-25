2d80503bb23cc20c2277decf51fe2ae849b90eb5
osasu <osasu.ogbebor@gmail.com>
2022-10-25 15:17:11 +0100

    the clean up before i get back to it

--- revision/projects/ch15/data_visualization/die_visual.py
+++ revision/projects/ch15/data_visualization/die_visual.py
@@ -9,4 +9,10 @@ for roll_num in range(100):
     result = die.roll()
     results.append(result)
 
-print(results)
+#analyze the results.
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
