33bed98f8f0f9e1795aea3b15e8c8047458e2926
osasu <osasu.ogbebor@gmail.com>
2025-01-15 23:29:34 -0500

    new commit

--- chapter4/mrfresh.py
+++
@@ -1,5 +0,0 @@
-# the items in the list should be between ''s , because they are strings.
-restaurant_foods = ('chicken', 'seafood', 'pasta', 'beef', 'lamb')
-print("Here are the foods this restaurant serves:")
-for restaurant_food in restaurant_foods:
-    print(restaurant_food)
